68fb2047df91d34479d5bd25848f582d1d6cecd8
Duy Trần <45138599+ferssken@users.noreply.github.com>
2021-01-14 15:55:39 +0700

    frist commit

---
+++ src/Model/AccountLogin.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author turtle
+ */
+public class AccountLogin implements Serializable{
+    private String email;
+    private String phone;
+    private String passWord;
+
+    public AccountLogin() {
+    }
+
+    public AccountLogin(String email, String phone, String passWord) {
+        this.email = email;
+        this.phone = phone;
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.email);
+        hash = 97 * hash + Objects.hashCode(this.passWord);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AccountLogin other = (AccountLogin) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return Objects.equals(this.passWord, other.passWord);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/Model/Car.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author turtle
+ */
+public class Car {
+    private String id;    // Số Khung
+    private String name;  // Tên Xe
+    private String brand; // Hãng
+    private float price;  // Giá
+    private String color; // mầu Xe
+    private byte seat;    // chỗ ngồi
+    private String year;  // Năm Sản Xuất
+
+    public Car() {
+    }
+
+    public Car(String id, String name, String brand, float price, String color, byte seat, String year) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+        this.seat = seat;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public byte getSeat() {
+        return seat;
+    }
+
+    public void setSeat(byte seat) {
+        this.seat = seat;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+}
